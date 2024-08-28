339736c64cbf63a390cdb089e2d689eede4d9a1e
Vitor Augusto <131685750+vitorAugusto2@users.noreply.github.com>
2024-08-28 15:46:59 -0300

    Delete easy/string directory

--- easy/string/3110. Score of a String.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def scoreOfString(self, s: str) -> int:
-        score = 0
-        for i in range(len(s) - 1):
-            score += abs(ord(s[i]) - ord(s[i + 1]))
-        return score
