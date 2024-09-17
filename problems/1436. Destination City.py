96cdfba7bd9e3aadef8a9899877b526449205f8d
vitorAugusto2 <vitor.alp20@outlook.com>
2024-09-17 12:17:03 -0300

    up

---
+++ problems/1436. Destination City.py
@@ -0,0 +1,13 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        city_start = set()
+        city_destintion = set()
+
+        for i, d in paths:
+            city_start.add(i)
+            city_destintion.add(d)
+
+        return list(city_destintion - city_start)[0]
+    
+
+# O(n) | O(n)
---
+++ problems/2710. Remove Trailing Zeros From a String.py
@@ -0,0 +1,22 @@
+class Solution:
+    def removeTrailingZeros(self, num: str) -> str:
+        return num.rstrip("0")
+
+# O(n) | O(n)
+
+"""
+Another approach
+----------------
+
+class Solution:
+    def removeTrailingZeros(self, num: str) -> str:
+        num_list = list(num)
+
+        while num_list[-1] == "0":
+            num_list.pop()    
+
+        return "".join(num_list)
+
+* O(n)
+"""
+
