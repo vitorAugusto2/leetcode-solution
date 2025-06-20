527312c02b0dad213c8009a693266493ca200084
vitorAugusto2 <vitor.alp20@outlook.com>
2025-06-20 16:13:58 -0300

    up

--- problems/1684. Count the Number of Consistent Strings.py
+++ problems/1684. Count the Number of Consistent Strings.py
@@ -1,19 +1,19 @@
-"""
-Approach
-----------------
+from typing import List
 
-1. 'allowed' is converted to a set
-2. Correct each word in the list
-3. Check whether the word set is a subset of the allowed set
-"""
 
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        set_allowed = set(allowed)  
+        set_allowed = set(allowed)   
         count = 0
 
         for word in words:
             if set(word) <= set_allowed:
+                print(word)
                 count += 1
-                
+
+
         return count
+
+
+sol = Solution()
+print(sol.countConsistentStrings("ab", ["ad","bd","aaab","baa","badab"]))
