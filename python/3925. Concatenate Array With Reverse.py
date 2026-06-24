641c3c3d6ae8d09907bca5958ba2fe35646e5c9a
vitorAugusto2 <vitor.alp20@outlook.com>
2026-06-24 10:23:31 -0300

    add solution

---
+++ python/3925. Concatenate Array With Reverse.py
@@ -0,0 +1,21 @@
+class Solution:
+    def concatWithReverse(self, nums: list[int]) -> list[int]:
+        return nums + nums[::-1]
+
+# Complexity (time:space): O(n) | O(n)
+
+"""
+Another Solution
+----------------
+
+n = len(nums)
+ans = [0] * (2*n)
+
+for i in range(n):
+    ans[i] = nums[i]
+    ans[2*n - 1 - i] = nums[i]
+
+return ans
+
+# Complexity (time:space): O(n) | O(n)
+"""
