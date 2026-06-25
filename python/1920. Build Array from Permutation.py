6372053e26cdcd077e3f4459d3566ea614b7b698
vitorAugusto2 <vitor.alp20@outlook.com>
2026-06-25 12:26:05 -0300

    python-solution-1920

--- python/1920. Build Array from Permutation.py
+++ python/1920. Build Array from Permutation.py
@@ -1,20 +1,31 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        ans = []
-        
-        for i in range(len(nums)):
-            ans.append(nums[nums[i]])
-        
+        ans = [0] * len(nums)
+        i = 1
+
+        for i in nums:
+            ans[i] = nums[nums[i]]
+
         return ans
-    
+
+# Time Complexity (time:space): O(n) | O(n)
+
 """
 Another Approach
 ----------------
 
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        return [nums[nums[i]] for i in range(len(nums))]
-        
-* Time complexity: O(n)
+        n = len(nums)
+
+        for i in range(n):
+            num_new = nums[nums[i] % n] % n
+            nums[i] = nums[i] + n * num_new
+
+        for i in range(n):
+            nums[i] = nums[i] // n
+
+        return nums
+
+# Time Complexity (time:space): O(n) | O(1)
 """
-    
